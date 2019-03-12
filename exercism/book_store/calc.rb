d5390e11ef24b8e5dc7b44b6cb2c10b9d87313fb
Katelyn <katelyn.rimkunas@gmail.com>
2019-03-11 20:31:59 -0600

    Basics of a runner file - user can add books to cart

--- exercism/book_store/calc.rb
+++ exercism/book_store/calc.rb
@@ -1,1 +1,25 @@
 # This is the runner file for the Book Store Challenge
+
+require 'pry'
+
+require './exercism/book_store/lib/store'
+require './exercism/book_store/lib/cart'
+
+# --------------
+
+# Welcome to Bookz
+bookz = Store.new
+bookz.add_series(5, "Stuff", 8)
+
+display = bookz.books
+
+# Hello
+person = Cart.new
+person.add_book( person.browse(display) )
+person.add_book( person.browse(display) )
+person.add_book( person.browse(display) )
+person.add_book( person.browse(display) )
+person.add_book( person.browse(display) )
+person.add_book( person.browse(display) )
+
+binding.pry
